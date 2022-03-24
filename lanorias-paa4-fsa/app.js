23212d777c25840436deac0960c7f91b848b44da
alexandralanorias <97576137+alexandralanorias@users.noreply.github.com>
2022-03-23 23:19:07 -0400

    edited app.js and added app screenshots

--- lanorias-paa4-fsa/app.js
+++ lanorias-paa4-fsa/app.js
@@ -8,12 +8,20 @@
 // First things first, open xampp and start apache and mysql
 // Create a database in localhost/phpmyadmin named school_management_system
 // copy-paste the queries found in the school-schema.sql file in the sql tab
+// Create a .env file with the following format:
+// ---
+// DB_HOST = localhost
+// DB_NAME = school_management_system
+// DB_USER = root
+// DB_PASS = ''
+// --
 // Then edit the .env file according to your username and password that you use in phpmyadmin
 // We'll be using port 5000 for this activity. If your port 5000 is already used go to this app.js file and find 'const PORT = process.env.PORT || 5000;'
 // Edit '5000' with whatever port you want to replace it with
 // Open the lanorias-paa4-fsa folder in your file explorer
 // Shift + Right click and select 'Open powershell windows here'
 // Type 'npm install'
+// You may see '1 high severity vulnerability' after installing, just ignore it as the app will still run
 // Then type 'npm start'
 // You're good to go! You should be seeing the 'Yay, no errors! Try accessing localhost: 5000 on your browser.' notice.
 // Lastly, go check out the CRUD app in your browser by going to localhost:5000 :)
